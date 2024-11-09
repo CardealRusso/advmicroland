67f16ce43f644fcf23de9ba5554ad9310dde9ecc
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2024-11-08 21:24:27 -0300

    Add files via upload

--- src/game_client.py
+++ src/game_client.py
@@ -3,13 +3,16 @@ import time
 import json
 import uwebsockets.client
 
-class Player:
+class Entity:
   def __init__(self, data):
     self.update(data)
   
   def update(self, data):
     for key, value in data.items():
       setattr(self, key, value)
+  
+  def __getitem__(self, key):
+    return getattr(self, key)
 
 class GameClient:
   def __init__(self, server, user, character, auth, width=1440, height=900, scale="2"):
@@ -20,8 +23,10 @@ class GameClient:
     self.width = width
     self.height = height
     self.scale = scale
-
-    self.websocket = uwebsockets.client.connect(f'wss://{server}.adventure.land:2053/socket.io/?EIO=4&transport=websocket')
+    
+    self.websocket = uwebsockets.client.connect(
+      f'wss://{server}.adventure.land:2053/socket.io/?EIO=4&transport=websocket'
+    )
     print("Connected")
     print("Sending hello to server..")
     self.websocket.send("40")
@@ -31,10 +36,14 @@ class GameClient:
     self.sent_auth = False
     self.sent_loaded = False
     self._running = True
+    
     _thread.start_new_thread(self.main_loop, ())
     print("Waiting for player data...")
     while not self.player:
       time.sleep(0.1)
+
+  def __getitem__(self, key):
+    return self.entities[key]
 
   def main_loop(self):
     while self._running:
@@ -44,16 +53,17 @@ class GameClient:
           self.websocket.send("3")
         if resp.startswith("42["):
           data = json.loads(resp[2:-1])
-          if data[0] == "welcome" and not self.sent_loaded:self._send_screen_info()
-          elif not self.sent_auth:self._send_auth()
+          if data[0] == "welcome" and not self.sent_loaded:
+            self._send_screen_info()
+          elif not self.sent_auth:
+            self._send_auth()
           elif data[0] == "entities" and self.sent_auth:
             self._update_entities(data[1])
             if self.player is None:
-              # This triggers player data
               self.websocket.send('42["use",{"item":"hp"}]')
           elif data[0] == "player" and self.sent_auth:
             if not self.player:
-              self.player = Player(data[1])
+              self.player = Entity(data[1])
             else:
               self.player.update(data[1])
 
@@ -83,13 +93,25 @@ class GameClient:
     self.sent_auth = True
 
   def _update_entities(self, data):
-    self.entities["players"].update({p["id"]: p for p in data.get("players", [])})
-    self.entities["monsters"].update({m["id"]: m for m in data.get("monsters", [])})
+    for entity_type in ["players", "monsters"]:
+      entities = data.get(entity_type, [])
+      self.entities[entity_type].update({
+        e["id"]: Entity(e) for e in entities
+      })
 
-  def move(self, x, y):
-    if self.player:
-      move_message = f'42["move",{{"x":{self.player.x},"y":{self.player.y},"going_x":{x},"going_y":{y},"m":0}}]'
-      self.websocket.send(move_message)
+  def move(self, *args):
+    """
+    Move to a position. Can be called in two ways:
+    - move(x, y)
+    - move(entity)
+    """
+    if len(args) == 2:
+      x, y = args
+    else:
+      x, y = args[0].x, args[0].y
+
+    move_message = f'42["move",{{"x":{self.player.x},"y":{self.player.y},"going_x":{x},"going_y":{y},"m":0}}]'
+    self.websocket.send(move_message)
 
   def running(self):
     time.sleep(0.5)
